9b07fe23d13e8de0ba90bf627345b552082bd170
hungcao14599 <hungnguyen14599@gmail.com>
2021-11-27 12:19:26 +0700

    handle-logic-sign-up-in

--- src/stores/api/auth.js
+++ src/stores/api/auth.js
@@ -3,13 +3,10 @@ import http from "../../services/http";
 const PREFIX = "/auth";
 
 export default class Auth {
-    static loginUser(username, password) {
-        return http.post(`${PREFIX}/login/`, { username, password });
+    static loginUser(email, password) {
+        return http.post(`${PREFIX}/login/`, { email, password });
     }
     static registerUser(username, password, email) {
         return http.post(`${PREFIX}/register/`, { username, password, email });
-    }
-    static verifyAccount(verifyCode, email) {
-        return http.post(`${PREFIX}/verify`, { verifyCode, email });
     }
 }
